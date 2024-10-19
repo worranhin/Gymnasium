47a1d7308b3f4344242dc08b228bcc40519cb6c5
worranhin <worranhin@foxmail.com>
2024-10-19 21:32:01 +0800

    add: several tutorial

---
+++ CustomEnv.py
@@ -0,0 +1,88 @@
+from typing import Optional
+import numpy as np
+import gymnasium as gym
+
+
+class GridWorldEnv(gym.Env):
+
+    def __init__(self, size: int = 5):
+        # The size of the square grid
+        self.size = size
+
+        # Define the agent and target location; randomly chosen in `reset` and updated in `step`
+        self._agent_location = np.array([-1, -1], dtype=np.int32)
+        self._target_location = np.array([-1, -1], dtype=np.int32)
+
+        # Observations are dictionaries with the agent's and the target's location.
+        # Each location is encoded as an element of {0, ..., `size`-1}^2
+        self.observation_space = gym.spaces.Dict(
+            {
+                "agent": gym.spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "target": gym.spaces.Box(0, size - 1, shape=(2,), dtype=int),
+            }
+        )
+
+        # We have 4 actions, corresponding to "right", "up", "left", "down"
+        self.action_space = gym.spaces.Discrete(4)
+        # Dictionary maps the abstract actions to the directions on the grid
+        self._action_to_direction = {
+            0: np.array([1, 0]),  # right
+            1: np.array([0, 1]),  # up
+            2: np.array([-1, 0]),  # left
+            3: np.array([0, -1]),  # down
+        }
+
+    def _get_obs(self):
+        return {"agent": self._agent_location, "target": self._target_location}
+
+    def _get_info(self):
+        return {
+            "distance": np.linalg.norm(
+                self._agent_location - self._target_location, ord=1
+            )
+        }
+
+    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
+        # We need the following line to seed self.np_random
+        super().reset(seed=seed)
+
+        # Choose the agent's location uniformly at random
+        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+
+        # We will sample the target's location randomly until it does not coincide with the agent's location
+        self._target_location = self._agent_location
+        while np.array_equal(self._target_location, self._agent_location):
+            self._target_location = self.np_random.integers(
+                0, self.size, size=2, dtype=int
+            )
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        return observation, info
+
+    def step(self, action):
+        # Map the action (element of {0,1,2,3}) to the direction we walk in
+        direction = self._action_to_direction[action]
+        # We use `np.clip` to make sure we don't leave the grid bounds
+        self._agent_location = np.clip(
+            self._agent_location + direction, 0, self.size - 1
+        )
+
+        # An environment is completed if and only if the agent has reached the target
+        terminated = np.array_equal(self._agent_location, self._target_location)
+        truncated = False
+        reward = (
+            1 if terminated else 0
+        )  # the agent is only reached at the end of the episode
+        observation = self._get_obs()
+        info = self._get_info()
+
+        return observation, reward, terminated, truncated, info
+
+
+# Register the environment
+gym.register(
+    id="gymnasium_env/GridWorld-v0",
+    entry_point=GridWorldEnv,
+)
